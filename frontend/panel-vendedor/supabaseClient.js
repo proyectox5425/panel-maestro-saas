285b7b75a92e2073f2401d379109303e0e0113e7
proyectox5425 <antonioleonalva.1994@gmail.com>
2025-08-13 16:41:34 -0400

    Delete frontend/panel-vendedor/supabaseClient.js

--- frontend/panel-vendedor/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-// supabaseClient.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
-
-export const supabase = createClient(
-  'https://xhqhtzrcohovylcxibkf.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhocWh0enJjb2hvdnlsY3hpYmtmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4Njg3NTEsImV4cCI6MjA3MDQ0NDc1MX0.aTAOD-m3n2n6MAg1Cx2lDOPAD4jT2z4bcQXjxZwgllI'
-);
